278bc0aff76bd3908d374643d5ede8db14a34fda
Oliver Staadt <ostaadt@gmail.com>
2018-11-14 14:47:35 +0100

    Build site at 2018-11-14 13:47:35 UTC

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "EVOCATION has officially started",
+        "excerpt":"The Marie Skłodowska-Curie Innovative Training Network Advanced Visual and Geometric Computing for 3D Capture, Display, and Fabrication (EVOCATION) has officially started on October 1, 2018. For more information go here.  ","categories": [],
+        "tags": [],
+        "url": "https://evocation.eu/evocation-started/",
+        "teaser":null}]
