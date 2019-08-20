e6be67a9a449d455c4efaa18d12c86a920f3c774
Oliver Staadt <ostaadt@gmail.com>
2019-08-20 12:22:27 +0200

    Build site at 2019-08-20 10:22:27 UTC

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "EVOCATION has officially started",
         "excerpt":"The Marie Skłodowska-Curie Innovative Training Network Advanced Visual and Geometric Computing for 3D Capture, Display, and Fabrication (EVOCATION) has officially started on October 1, 2018. For more information go here.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/news/2018-10-01-evocation-started/",
+        "url": "https://evocation.eu/news/2018-10-01-evocation-started/",
         "teaser":null},{
         "title": "EVOCATION MSCA Innovative Training Network: 14 Early-Stage Researcher (ESR) Fellowships – 3-year PhD Positions",
         "excerpt":"We are happy to announce the availability of 14 Early-Stage Researcher (ESR) PhD positions in Advanced Visual and Geometric Computing for 3D Capture, Display, and Fabrication (EVOCATION), a Marie Skłodowska-Curie Innovative Training Network. EVOCATION officially started in October 2018. These PhDs are early-stage scientific and industrial researchers who in the...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/news/2018-12-17-evocation-recruitment/",
+        "url": "https://evocation.eu/news/2018-12-17-evocation-recruitment/",
         "teaser":null},]
