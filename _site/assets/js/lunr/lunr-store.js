9990800eef28f2a84447a9de301d54f3ae146f6f
Tariqul Islam <ai039@informatik.uni-rostock.de>
2020-06-03 17:48:52 +0200

    Build site at 2020-06-03 15:48:52 UTC

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,18 +2,18 @@ var store = [{
         "title": "EVOCATION has officially started",
         "excerpt":"The Marie Skłodowska-Curie Innovative Training Network Advanced Visual and Geometric Computing for 3D Capture, Display, and Fabrication (EVOCATION) has officially started on October 1, 2018. For more information go here.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/news/2018-10-01-evocation-started/",
+        "url": "https://evocation.eu/news/2018-10-01-evocation-started/",
         "teaser": null
       },{
         "title": "EVOCATION MSCA Innovative Training Network: 14 Early-Stage Researcher (ESR) Fellowships – 3-year PhD Positions",
         "excerpt":"We are happy to announce the availability of 14 Early-Stage Researcher (ESR) PhD positions in Advanced Visual and Geometric Computing for 3D Capture, Display, and Fabrication (EVOCATION), a Marie Skłodowska-Curie Innovative Training Network. EVOCATION officially started in October 2018. These PhDs are early-stage scientific and industrial researchers who in the...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/news/2018-12-17-evocation-recruitment/",
+        "url": "https://evocation.eu/news/2018-12-17-evocation-recruitment/",
         "teaser": null
       },{
         "title": "EVOCATION first winter school in Vienna, Austria",
         "excerpt":"Winter school in Vienna, Austria. The first winter school of EVOCATION took place in Vienna, Austria from 27-29 November, 2019. The winter school is the first time that the whole EVOCATION network was brought together and that all the Early Stage Researchers (ESRs) and Project Leaders (PIs) got a chance...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/news/2019-11-30-evocation-winter-school/",
+        "url": "https://evocation.eu/news/2019-11-30-evocation-winter-school/",
         "teaser": null
       },]
