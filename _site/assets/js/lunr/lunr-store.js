585d390f40a105856fa806e4453308b5471ecaef
Tariqul Islam <ai039@informatik.uni-rostock.de>
2021-03-08 11:37:04 +0100

    Added new news in the news page

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -34,4 +34,10 @@ var store = [{
         "tags": [],
         "url": "/news/2021-03-08-EVOCATION-participation-support-and-contribution-EGPGV-EuroVis/",
         "teaser": null
+      },{
+        "title": "EVOCATION's participation, support and contribution to the EGPGV and EuroVis 2021",
+        "excerpt":"EVOCATION is planning to participate in as well as support and contribute to the Eurographics Symposium on Parallel Graphics and Visualization (EGPGV) and Eurographics Visualization Conference (EuroVis) 2021, both held in Zürich (as virtual conferences online) and organized by the project partner UZH. For details of these events, please check...","categories": [],
+        "tags": [],
+        "url": "/news/2021-06-14-EVOCATION%20participation,%20support%20and%20contribution%20to%20the%20%20EGPGV%20%20and%20EuroVis/",
+        "teaser": null
       },]
